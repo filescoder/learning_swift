ff1e19d16828e47a0768f39c011169da2ea07115
Nicola Fabiani <>
2018-11-19 13:39:03 +0100

    Setting Up

---
+++ project_01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project_01
+//
+//  Created by Nicola Fabiani on 19/11/2018.
+//  Copyright © 2018 Nicola Fabiani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
